5d03c08377f8643f9009cda3124cb3288ee17acf
casares <casares@Equipo>
2015-06-17 17:13:26 +0200

    Refactorizacion main de test

---
+++ PredictPok01/src/com/predictpok/poker/PredictPreflopTest.java
@@ -0,0 +1,14 @@
+package com.predictpok.poker;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PredictPreflopTest {
+
+	@Test
+	public void testPrediccionPreflop() {
+		fail("Not yet implemented");
+	}
+
+}
--- PredictPok01/src/com/predictpok01/principal/Aplicacion.java
+++ PredictPok01/src/com/predictpok01/principal/Aplicacion.java
@@ -9,27 +9,6 @@ import com.predictpok.poker.*;
 public class Aplicacion {
 
 	public static void main(String[] args) throws SQLException {
-		/*ControlDB database = new ControlDB();
-		database.conectar();
-		String consulta="SELECT COUNT(*) AS numero FROM Preflop WHERE id_jugador='doyle'";
-		ResultSet tabla= database.consultar(consulta);
-		
-		try {
-			if (tabla.next()) System.out.println("tiene elementos");
-			int numero_elementos=Integer.parseInt(tabla.getString("numero"));
-			System.out.println(numero_elementos);
-			consulta="SELECT DISTINCT mano_preflop FROM Preflop "
-					+ "WHERE id_jugador='doyle' "
-					+ "GROUP BY mano_preflop";
-			tabla=database.consultar(consulta);
-			
-		} catch (SQLException e) {
-			e.printStackTrace();
-			System.out.println(e.getMessage());
-		}
-	
-		
-		database.desconectar();*/
 		String[] apuestas={"call","raise"};
 		PredictPreflop predic=null;
 		try {
@@ -38,24 +17,7 @@ public class Aplicacion {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		//System.out.println(predic.getTotalJugadas());
-		//System.out.println(predic.toStringFrecuencias());
-		/*ArrayList<ManoPreflop> manos =null;
-		try {
-			manos=predic.getMasFrecuentes(3);
-			for(int i=0;i<manos.size();i++){
-				if(i==0)System.out.println("Prediccion: "+manos.get(i).toString() );
-				System.out.println(manos.get(i).toString());
-			}
-			System.out.println("Numero de filas: "+predic.numeroFilas());
-			predic.mostrarFrecuencias();
-			System.out.println("Media: "+predic.media());
-			//System.out.println("Desviacion: "+predic.desviacion());
-			//System.out.println(predic.fiabilidad());
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}*/
+		
 		System.out.println("Total Jugadas: "+predic.getTotalJugadas());
 		System.out.println("Numero de filas: "+predic.numeroFilas());
 		System.out.println("Tabla de frecuencias");
@@ -84,7 +46,7 @@ public class Aplicacion {
 			System.out.println("No hay suficiente informacion en la BD");
 		}
 		System.out.println("Proximo Movimiento: "+predic.sigMovimiento());
-		//System.out.println(predic.media());
+	
 	}
 
 }
